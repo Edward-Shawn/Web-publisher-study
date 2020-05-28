bed2bb1b5b43da988b6600860edc40a9f6480b21
Edward-Shawn <61370305+Edward-Shawn@users.noreply.github.com>
2020-05-28 17:39:46 +0900

    제이쿼리를 활용한 메뉴 액션 구현

--- js/index.js
+++ js/index.js
@@ -9,6 +9,27 @@ $(document).ready(function(){
       $(".tabs>button").removeClass("active");
       $(this).addClass("active");
     });
+
     $(".tabs>button:nth-child(1)").trigger("click");
+
+
+    $(".hamberger").click(function(){
+
+        $(this).toggleClass("on");
+
+        if($(this).hasClass("on")){
+            //console.log("true");
+            $(".hamberger>.fa").attr("class","fa fa-times");
+            $(".gnb").slideDown("fast");
+        } else {
+            //console.log("false");
+            $(".hamberger>.fa").attr("class","fa fa-bars");
+            $(".gnb").slideUp("fast");
+        }
+
+    });
+
+
+
 
   });
