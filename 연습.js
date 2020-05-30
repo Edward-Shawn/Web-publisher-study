9810b5116e3780b5b4ec0b54a7fe6f7b15583c58
Edward-Shawn <61370305+Edward-Shawn@users.noreply.github.com>
2020-05-30 15:30:57 +0900

    제이쿼리를 이용한 슬라이드다운 메뉴 구현

--- 연습.js
+++ 연습.js
@@ -5,11 +5,12 @@ $(document).ready(function(){
         if($(this).hasClass("on")){
             // console.log("ture");
             $(".hamberger>.fa").attr("class","fa fa-times");
+            $(".menu").slideDown('fast');
 
         }else{
             // console.log("false");
             $(".hamberger>.fa").attr("class","fa fa-bars");
-            
+            $(".menu").slideUp('fast');
         }
 
     });
