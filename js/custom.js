1cc88271faaf9545d64128b8c7309860063aff97
Edward-Shawn <61370305+Edward-Shawn@users.noreply.github.com>
2020-06-18 14:54:49 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    // var winWidth = $(window).width() * 0.35;
+        $(".main_img").height($(window).width() * 0.35);
+
+    $(window).resize(function(){
+        var winWidth = $(window).width() * 0.35;
+        $(".main_img").height(winWidth);
+        // 윈도우의 길이 값을  winWidth에 대입한다.
+    });
+
+    //menu icon click event
+    $(".nav_icon").click(function(){
+        $(this).toggleClass("on");
+        $("header .gnb")
+
+        if($(this).hasClass("on")){
+            $(".gnb").slideDown();
+        } else {
+            $(".gnb").slideUp();
+        }
+    });
+
+
+    // header stick when mouse scrolling down
+    let hedTop = $("header").offset().top;
+    $(window).scroll(function(){
+       let scroll = $(window).scrollTop();
+
+       // $("h4").text(scroll);
+       if(scroll>=hedTop){
+        $("header").addClass("scroll");
+       }else{
+           $("header").removeClass("scroll");
+       }
+    });
+
+    $(".gnb li ").click(function(){
+      let valuePos = $(this).find("a").attr("value");
+      let offsetTop = $("#"+valuePos).offset().top;
+        //alert(valuePos)
+        $("html, body").animate({scrollTop:offsetTop -70}, 500);
+    });
+
+
+});
