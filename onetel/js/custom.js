6ee44b567a5afc7b0694999e834854894907e7e2
Edward-Shawn <61370305+Edward-Shawn@users.noreply.github.com>
2020-06-18 14:02:43 +0900

    수업용 데모사이트 제작

--- onetel/js/custom.js
+++ onetel/js/custom.js
@@ -18,8 +18,27 @@ $(document).ready(function(){
         } else {
             $(".gnb").slideUp();
         }
-
-
     });
+
+
+    // header stick when mouse scrolling down
+    let hedTop = $("header").offset().top;
+    $(window).scroll(function(){
+       let scroll = $(window).scrollTop();
+
+       // $("h4").text(scroll);
+       if(scroll>=hedTop){
+        $("header").addClass("scroll");
+       }else{
+           $("header").removeClass("scroll");
+       }
+    });
+
+    $(".gnb li a").click(function(){
+        let secPosition = $(this).attr("value");
+       
+        // alert(moveScreen);
+    });
+
 
 });
