d263ad7ac074290c99abfdbc6678dd3e8e5c3eef
Edward-Shawn <61370305+Edward-Shawn@users.noreply.github.com>
2020-06-26 14:52:27 +0900

    홈페이지 제작 완료-  제이쿼리 로딩 이벤트 구현

--- js/custom.js
+++ js/custom.js
@@ -8,18 +8,24 @@ $(document).ready(function(){
        imgs += '<img src="img/pic' + i + '.jpg">'
    }
 
-   $('section').html(imgs);
+//    $('section').html(imgs);
 
-   $("body").on("mousemove", function(e){
-        var posX = e.pageX;
-        var winWidth = $(window).width();
-        var calcWidth = Math.floor((posX/winWidth)* 200);
-        //floor() 함수는 소수점 이하를 버린다
-        //https://www.w3schools.com/js/js_math.asp
-        $("h3").text(calcWidth);
-        $("section>img").hide();
-        $("section>img").eq(calcWidth).show();
-   });
+//    $("body").on("mousemove", function(e){
+//         var posX = e.pageX;
+//         var winWidth = $(window).width();
+//         var calcWidth = Math.floor((posX/winWidth)* 200);
+//         //floor() 함수는 소수점 이하를 버린다
+//         //https://www.w3schools.com/js/js_math.asp
+//         $("h3").text(calcWidth);
+//         $("section>img").hide();
+//         $("section>img").eq(calcWidth).show();
+//    });
 
+   $("body").jpreLoader({
+       splashID:"#jSplash",
+       loaderVPos: '48%',
+       autoClass:true,
+       closeBtnText:"let's Begin!"
+   })
 
 });
