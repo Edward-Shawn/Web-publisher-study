d85454476a496d98892ae7cf1b12a0db990e06a9
Edward-Shawn <61370305+Edward-Shawn@users.noreply.github.com>
2020-06-26 14:55:49 +0900

    세로 스크롤 이벤트 페이지 제작

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,12 @@
 $(document).ready(function(){
+    // $(window).scroll(function(){
+    //   var scroll = $(window).scrollTop();
+    //   $("section").stop().animate({
+    //       left: -scroll
+    //   },200);
+    // });
+    
 
-   var imgs = '';
-
-   for(i=0; i< 200; i++){
-       //imgs = i;
-       //console.log(imgs);
-       imgs += '<img src="img/pic' + i + '.jpg">'
-   }
-
-//    $('section').html(imgs);
-
-//    $("body").on("mousemove", function(e){
-//         var posX = e.pageX;
-//         var winWidth = $(window).width();
-//         var calcWidth = Math.floor((posX/winWidth)* 200);
-//         //floor() 함수는 소수점 이하를 버린다
-//         //https://www.w3schools.com/js/js_math.asp
-//         $("h3").text(calcWidth);
-//         $("section>img").hide();
-//         $("section>img").eq(calcWidth).show();
-//    });
-
-   $("body").jpreLoader({
-       splashID:"#jSplash",
-       loaderVPos: '48%',
-       autoClass:true,
-       closeBtnText:"let's Begin!"
-   })
+    
 
 });
