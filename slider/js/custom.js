3befceb9e1568b1f3da567be5805c0c923eba763
Edward-Shawn <61370305+Edward-Shawn@users.noreply.github.com>
2020-07-03 18:03:50 +0900

    제이쿼리를 활용한 슬라이드 페이지 제작

--- slider/js/custom.js
+++ slider/js/custom.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     var mySlide = function(){
         //variation hoisting   - 변수도 2가지로 만들것  가져오는 변수 1 / 행하는 변수 1 
 
-        //1. document selectors
+        //1. document selectors  ( 앞에 $ 붙이는 패턴 문서내에 있는걸 불러왔다는 뜻)
         var $conainer = $(".slideshow");
         var $slideGroup = $conainer.find(".slideshow-slides");
         var $slides = $slideGroup.find(".slide");
@@ -14,26 +14,29 @@ $(document).ready(function(){
         
         //2. normal variation
         var currentIndex = 0;
-        var timer;
+        var timer; // 객체타입
+        var indicatorHTML = ''; // 원시타입
 
         //--------------------------------------------------------------------------
 
         //document contrll
         //1. setting image's left value
+        //2. insert the indicator's "a" tags equal to picture count
         for(var i = 0; i < $slides.length; i++){
             $slides.eq(i).css({ left: 100 * i + "%" }); 
+            indicatorHTML += '<a href="#"></a>';
             // left 100의 디폴트는 px 이지만 뒤에 %를 붙여서 단위를 바꿔줌
-
-
         }
 
+        $indicator.html(indicatorHTML);
 
         //define functions
         //1. main slide funtion
-        function goToSlide(index){
+        function goToSlide(index/*파라미터*/){
             $slideGroup.animate({left: -100 * index + "%"}, 400);
             currentIndex = index;
-            update()
+            //update();            
+            indicatorBtn();  
         }
 
         //2. auto slide start function
@@ -42,7 +45,7 @@ $(document).ready(function(){
                 var nextIndex = (currentIndex + 1 ) % $slides.length;   // 슬라이드 제작시 많이 쓰는 공식
                 goToSlide(nextIndex);
                 console.log(nextIndex);
-            }, 2000)
+            }, 3000)
         }
 
         //3. auto slide stop funtion
@@ -51,8 +54,9 @@ $(document).ready(function(){
         }
 
         //4. stop slide when slide is in first or last index
-        function update(){
-            if(currentIndex === 0){
+
+        /*function update(){
+            if(currentIndex === 0){         // 첫번째 선언
                 $nav.find(".prev").hide();
             }else{
                 $nav.find(".prev").show();
@@ -60,14 +64,18 @@ $(document).ready(function(){
 
 
             
-            if(currentIndex === $slides.length  - 1 ){
+            if(currentIndex === $slides.length  - 1 ){   // 마지막 선언
                 $nav.find(".next").hide();
             }else{
                 $nav.find(".next").show();
             }
+        }*/
+
+        function indicatorBtn (){
+            $indicator.find("a").removeClass("active");
+            $indicator.find("a").eq(currentIndex).addClass
+            ("active");
         }
-
-
 
         //document selector controll
         //.1 stop or start slide when mouse hover or out
@@ -83,12 +91,31 @@ $(document).ready(function(){
         $nav.on("click", "a", function(e){
             e.preventDefault();
             if ($(this).hasClass("prev")){
-                goToSlide(currentIndex - 1);
+
+                if(currentIndex === 0){
+                    return false;
+                }else{
+                    goToSlide(currentIndex - 1);
+                }
+
             }else{
-                goToSlide(currentIndex + 1);
+
+                if(currentIndex === $slides.length -1 ){
+                    return false;
+                }else{
+                    goToSlide(currentIndex + 1);
+                }
+
             }
         });
 
+
+        //3. slide moving to clicked indicator's index
+        $indicator.on("click", "a", function (e){
+            e.preventDefault();
+            var index = $(this).index();
+            goToSlide(index);
+        });
 
 
         //active functions
@@ -97,7 +124,7 @@ $(document).ready(function(){
 
     }
     
-    mySlide();
+    mySlide();  //선언
 
 });
 
