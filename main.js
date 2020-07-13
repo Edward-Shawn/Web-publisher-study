e77dfd0afefc6b28dd02b4cc7bf2cfd4ced36f79
Edward-Shawn <61370305+Edward-Shawn@users.noreply.github.com>
2020-07-13 18:27:34 +0900

    부트스트랩을 이용한 웹사이트 제작

--- main.js
+++ main.js
@@ -1,5 +1,91 @@
 $(document).ready(function () {
     
+
+    // scrolling up to top when clicking button
+    var backToTOP = function(){
+        $(window).scroll(function(){
+            var scroll = $(window).scrollTop();
+            if(scroll > 100){
+                $(".back-to-top").fadeIn();
+            }else{
+                $(".back-to-top").fadeOut();
+            }
+        });
+        $(".back-to-top").click(function(){
+            $("html,body").stop().animate({scrollTop:0},800)
+        });
+    }
+
+    backToTOP();
+
+    var activateMenu = function(){
+        var sections = $("section");
+        var nav = $(".nav-menu");
+        
+        //섹션 위치에 따른 li 의 active 적용 기능
+        $(window).scroll(function(){
+            var scroll = $(window).scrollTop()+200;
+            sections.each(function(){
+                var top = $(this).offset().top;
+                var bottom = top + $(this).outerHeight();
+                var now_section = $(this).attr("id");
+                console.log(now_section);
+                if(scroll >= top && scroll < bottom){
+                    nav.find("li").removeClass("menu-active");
+                    nav.find("a[href='#"+ now_section +"']").parent
+                    ("li").addClass("menu-active"); // services 양쪽에 +를 넣음으로써 자바스크립트 언어로 변경
+                }
+                if(scroll < 300){
+                    $(".nav-menu li:first-child").addClass("menu-active");
+                }
+            });
+        });
+    }
+    activateMenu(); //함수호출
+
+
+
+    // function goTop(){
+    //     $('html, body').scrollTop(0);
+    //     // scrollTop 메서드에 0 을 넣어서 실행하면 끝 !!
+    //     // 간혹 이 소스가 동작하지 않는다면
+    //     // $('html, body') 로 해보세요~
+    // }
+    
+
+    // header sticky when scrolling down
+    // scroll move to each section when clicking menu
+    $(".nav-menu li").click(function(){
+        var target = $(this).find("a").attr("href");
+
+        var offsetTop = $(target).offset().top;
+        var headHeight = $("#header").outerHeight() -1;   //outerHeight은 패딩값을 포함하여 높이를 잡음 -1은 추가 높이값
+        $("html,body").stop().animate({scrollTop:offsetTop - headHeight},500);
+    });
+    
+
+
+    $("#header").sticky({
+        topSpacing:0,
+        zIndex:'999',
+    });
+
+    //using hard coding
+    // var stickyheader = function(){
+    //     var headTop = $("#header").offset().top; //지역변수말고 전역 변수로 적용해야한다.
+    //     $(window).scroll(function(){
+    //     var scroll = $(window).scrollTop();
+                
+    //     if(scroll >= headTop){
+    //         $("#header").addClass("sticky");
+    //     }else{
+    //         $("#header").removeClass("sticky");
+    //     }
+            
+    //     });
+    // }
+    // stickyheader();
+
     //intro maun slide carousel
     $("#intro-carousel").owlCarousel({
         autoplay: true,
